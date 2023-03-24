686be7bf13ba9225a373f0c185c43f60f201fd65
AardentNafiz <92028026+NafizHaque@users.noreply.github.com>
2023-03-24 15:27:22 +0000

    Post

---
+++ ExampleMVC/ExampleMVC/Controllers/StartController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExampleMVC.Models;
+
+namespace ExampleMVC.Controllers
+{
+    public class StartController : Controller
+    {
+
+        private static List<DogViewModel> dogs= new List<DogViewModel>();
+
+        public IActionResult Index()
+        {   
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var newDogViewModel = new DogViewModel();
+            return View(newDogViewModel);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ ExampleMVC/ExampleMVC/Models/DogViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleMVC.Models
+{
+    public class DogViewModel
+    {
+        public string name { get; set; }
+
+        public int age { get; set; } 
+    }
+}
